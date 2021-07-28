76e203888799b140bc3d4e0848ff318d5bb3f951
Meyerka <40023031+Meyerka@users.noreply.github.com>
2021-07-28 15:14:11 +0200

    Feature: Initialized apollo gql server

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,26 @@
 import express = require('express');
+import { ApolloServer, gql } from 'apollo-server-express';
+
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+// Provide resolver functions for your schema fields
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world!',
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
 
 const app = express();
-
-
 app.use(express.json())
+server.applyMiddleware({ app });
+
 
 
 
