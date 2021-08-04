bbb5b391759dde2cce29d729b381a31c3605aaac
Karl-Erik Meyer <karlerik.meyer@protime.eu>
2021-08-04 15:43:03 +0200

    Feature: Finished basic gql schema

--- src/graphql/schema/index.ts
+++ src/graphql/schema/index.ts
@@ -3,7 +3,27 @@ import {buildSchema} from 'graphql';
 module.exports = buildSchema(`
     type Sale {
         _ID: ID!
-        typeOfItem: string
-        price: 
+        typeOfItem: String!
+        price: Number!
+        location: String!
+    }
+
+    input SaleInput {
+        typeOfItem: String!
+        price: Number!
+        location: String!
+    }
+
+    type Query {
+        sales: [Sale!]
+    }    
+
+    type Mutation {
+        createSale(sale:Sale): Sale
+    }
+
+    schema {
+        query: Query
+        mutation: Mutation
     }
 `)
